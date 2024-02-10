46c29e6d3bfc338eec655c6d869ba3cf814770ee
Dima Birenbaum <dvlasenko86@gmail.com>
2024-02-10 19:40:47 +0200

    Added LayerNormalization

--- model.py
+++ model.py
@@ -51,6 +51,8 @@ class PositionalEncoding(nn.Module):
         # Create a matrix shape (seq_len, d_model)
         pe = torch.zeros(seq_len, d_model)
         position = torch.arange(0, seq_len - 1, dtype=torch.float).unsqueeze(1)
+        
+        # Calculate the div_term for the positional encoding function (2i) and (2i+1) indices separately
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-torch.log(torch.tensor(10000.0)) / d_model))
 
         # Apply sine to even indices
@@ -76,3 +78,41 @@ class PositionalEncoding(nn.Module):
         """
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) # Add positional encoding to the input
         return self.dropout(x)
+
+class LayerNormalization(nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-6) -> None:
+        """
+        Initialize the LayerNormalization module.
+
+        Args:
+            d_model (int): The dimensionality of the model.
+            eps (float): A small value to prevent division by zero.
+
+        Returns:
+            None
+        """
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+
+        self.alpha = nn.Parameter(torch.ones(1)) # Learnable scale - Multiplied
+        self.bias = nn.Parameter(torch.zeros(1)) # Learnable bias - Added
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the LayerNormalization module.
+
+        Args:
+            x (torch.Tensor): The input tensor.
+
+        Returns:
+            torch.Tensor: The normalized input tensor.
+        """
+        # Calculate the mean of the input tensor over the last dimension (feature dimension) and keep the dimension for broadcasting
+        mean = x.mean(dim=-1, keepdim=True)
+        
+        # Calculate the standard deviation of the input tensor over the last dimension (feature dimension) and keep the dimension for broadcasting
+        std = x.std(dim=-1, keepdim=True)
+
+        # Normalize the input tensor and return it with the learnable scale and bias applied
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias 
